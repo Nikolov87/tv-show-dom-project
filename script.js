d5ed8b851fd6fe48c653f754b3e7f276663dfd93
Petar Nikolov <nikolov@Petars-MBP.broadband>
2022-11-13 22:01:08 +0000

    in progress

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 //You can edit ALL of the code here
 const rootElem = document.getElementById("root");
-console.log(rootElem);
-let tvshow = getAllEpisodes();
 
-tvshow.forEach((element) => {
-  // debugger;
-  console.log(element.name);
-  let title = document.createElement("h2");
-  title.innerText = element.name + element.season;
-  rootElem.appendChild(title);
-});
 
-// function setup() {
-//   const allEpisodes = getAllEpisodes();
-//   makePageForEpisodes(allEpisodes);
-// }
 
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
+// tvshow.forEach((element) => {
+//   // debugger;
+//   console.log(element.name);
+//   let title = document.createElement("h2");
+//   title.innerText = element.name + element.number;
+//   rootElem.appendChild(title);
+// });
 
-// window.onload = setup;
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+}
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(episode => {
+     
+    const episodeContainer = document.createElement("div");
+     episodeContainer.className = "episode-container"
+     rootElem.appendChild(episodeContainer)
+     
+     const episodeName = document.createElement("h4");
+     episodeName.innerText = `${episode.name} - ${episodeCodeFormatter(episode.season, episode.number)}`; 
+     episodeContainer.appendChild(episodeName);
+     
+     const episodeImg = document.createElement("img");
+     episodeImg.src = episode.image.medium;
+     episodeContainer.appendChild(episodeImg);
+
+     const episodeSummary = document.createElement("p");
+     episodeSummary.innerHTML = episode.summary;
+     episodeContainer.appendChild(episodeSummary);
+
+     const select = document.querySelector("select");
+     const option = document.createElement("option");
+     option.innerText = `${episodeCodeFormatter(episode.season, episode.number)} - ${episode.name}`;
+     select.appendChild(option);
+     
+  });
+
+}
+
+function episodeCodeFormatter(season, number) {
+  season = season < 10 ? "0" + season : season;
+  number = number < 10 ? "0" + number : number;
+  return `S${season}E${number}`;
+
+}
+
+
+window.onload = setup;
